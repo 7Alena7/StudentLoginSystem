be1b0085296aa0b3168cba19e4c1ebdb32d761cc
Alena <AlenaShmelkova@gmail.com>
2024-08-17 19:05:09 -0400

    -initial commit

---
+++ IntelliJ Projects/StudentLogin/src/main/java/com/example/StudentLogin/repository/StudentRepository.java
@@ -0,0 +1,8 @@
+package com.example.StudentLogin.repository;
+
+import com.example.StudentLogin.model.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StudentRepository extends JpaRepository<Student, Long> {
+    Student findByEmail(String email);
+}
